33d259b44a6f80bfde07757b17ba9be8460c6e44
Daniel Li <neolee11@yahoo.com>
2014-05-08 17:03:35 -0400

    Added domain project

---
+++ Aurora/Domain/CreditCardType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public enum CreditCardType
+    {
+        Unknown = 0,
+        Visa,
+        Master,
+        AmEx
+    }
+}
