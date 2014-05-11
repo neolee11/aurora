473636cf2e89326fc57d8ee4d547cf0d12643d69
neolee11 <neolee11@yahoo.com>
2014-05-10 20:20:37 -0400

    ShoppingCart_Test

---
+++ Aurora/Domain.Tests/ShoppingCart_Test/When_Customer_Add_To_ShoppingCart.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Tests.ShoppingCart_Test
+{
+    [TestClass]
+    public class When_Customer_Add_To_ShoppingCart
+    {
+        
+    }
+}
