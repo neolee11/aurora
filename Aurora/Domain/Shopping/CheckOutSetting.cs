7c7cd2b97944d4164526e0a6dc6e2d9c03141688
Daniel Li <neolee11@yahoo.com>
2014-05-15 12:27:11 -0400

    Add checkoutsetting class

---
+++ Aurora/Domain/Shopping/CheckOutSetting.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Shopping
+{
+    public class CheckOutSetting
+    {
+        public int CreditCardId { get; set; }
+        public IShippingMethod ShippingMethod { get; set; }
+
+    }
+}
