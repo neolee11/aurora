af85ab435e6eede27b47a3a5060e81e81ab28d09
neolee11 <neolee11@yahoo.com>
2014-05-15 21:21:30 -0400

    commit

--- Aurora/Domain.Tests/Shopping_Tests/CustomerOrder_Tests/When_Create_Order.cs
+++ Aurora/Domain.Tests/Shopping_Tests/CustomerOrder_Tests/When_Create_Order.cs
@@ -7,6 +7,7 @@ namespace Domain.Tests.Shopping_Tests.CustomerOrder_Tests
     [TestClass]
     public class When_Create_Order
     {
+        //here I can use mocks
         [TestMethod]
         public void Create_order_by_giving_shoppingcart_and_customer_and_creditcard_should_yield_a_successful_order()
         {
