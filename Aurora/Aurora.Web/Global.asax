542528f359f88927ff41bf9f329a97e112e06589
neolee11 <neolee11@yahoo.com>
2014-06-05 23:26:40 -0400

    add Aurora.Web

---
+++ Aurora/Aurora.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Aurora.Web.MvcApplication" Language="C#" %>
---
+++ Aurora/Aurora.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Aurora.Web.Startup))]
+namespace Aurora.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
